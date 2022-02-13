981925e1a4d25905b389a9c8b969331c80296ea2
theRDVN <rado.pieter@gmail.com>
2022-02-13 22:53:22 +0100

    Commit basic app

---
+++ app/src/test/java/euba/diplomovka/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package euba.diplomovka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
