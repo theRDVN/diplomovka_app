184d0b88e221fc405468f536885ac021943ae00b
Radovan Pieter <pieterr@posam.sk>
2022-02-19 02:35:06 +0100

    nova verzia, pridanie html stranky uvodnej

--- app/src/main/java/euba/diplomovka/app/AppController.java
+++ app/src/main/java/euba/diplomovka/app/AppController.java
@@ -1,14 +1,16 @@
 package euba.diplomovka.app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class AppController {
 
     @RequestMapping("/")
     public String getWelcomeMessage() {
-
-        return "Hello Docker World!";
+        return "index.html";
     }
 }
